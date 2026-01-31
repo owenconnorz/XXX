5d8b98150367ddafef9498d10e3635fae21deef0
Owen <96526481+owenconnorz@users.noreply.github.com>
2026-01-31 07:31:21 +0000

    Update Porntoalextension

--- Porntotal/src/main/kotlin/com/owencz1998/Porntoalextension.kt
+++ Porntotal/src/main/kotlin/com/owencz1998/Porntoalextension.kt
@@ -5,9 +5,9 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class PorntrexProvider : Plugin() {
+class Porntotalextension : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Porntrex())
+        registerMainAPI(Porntotal())
     }
 }
